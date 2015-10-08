2d06a66759237c138e190cdd742bd253a1a7d285
Samuel Rosenstein <smr277@cornell.edu>
2015-10-08 14:58:31 -0400

    actually kinda works now

--- latexsym.py
+++ latexsym.py
@@ -1,31 +1,9 @@
+from __future__ import division
 import sys
 from modgrammar import *
+import sympy
 
 grammar_whitespace_mode = 'optional'
-
-"""
-ParenExpr := (Expr)
-P0Term := ParenExpr | Number
-P0Expr := P0Term (/P0Term)*
-P1Term := P0Expr | ParenExpr | Number
-P1Expr := P1Term (*times*P1Term)*
-P2Term := P0Expr | P1Expr | ParenExpr | Number
-P2Expr := P2Term (+|- P2Term)*
-Expr := P2Expr | P1Expr | P0Expr | ParenExpr | Number
-"""
-
-"""
-\frac{2}{2^{8}}
-\frac{2}{9}^{5}
-
-Number := 
-ParenExpr := (Expr)
-BracketExpr := {Expr}
-ParsedExpr := \frac{Expr}{Expr}
-P0Term := ParenExpr | ParsedExpr | Number 
-P0Expr := P0Term ^ BracketExpr
-Expr := P0Expr | ParsedExpr | ParenExpr | Number
-"""
 
 
 """
@@ -41,6 +19,35 @@ P2Term := P1Expr | P1Term
 P2Expr := P2Term (+|- P2Term)k*
 Expr := P2Expr | P1Expr | P0Expr | P0Term
 """
+
+"""
+Vars needed for symbolic computation
+"""
+class SymParse():
+    def __init__(self):
+        self.symbolic_vars = {}
+        self.symbolic_var_list = []
+        
+    def add_sym_var(self, var):
+        self.symbolic_vars[var] = 1
+
+    def to_string(self):
+        sym_string = ""
+        for itm in self.symbolic_vars.keys(): 
+            sym_string+=" {}".format(itm)
+        return sym_string
+
+    def to_list(self):
+        return self.symbolic_vars.keys()
+
+    def create_symvars(self):
+        sympy.symbols(self.to_string())
+
+    def simplify(self, equation):
+        return sympy.simplify(equation)
+
+
+symParser = SymParse()
 
 """
 Items that need to be parsed using brackets
@@ -65,10 +72,16 @@ class Integral (Grammar):
     grammar = (L('\\integral_{'), REF('Expr'), L('}^{'), REF('Expr'), L('}{'), REF('Expr'), L('d'), REF('Var'), L('}'))
     
     def value(self):
-        return "integral({},({},{},{}))".format(self[5].value(), self[7].value(), self[1].value(), self[3].value())
+        return "integrate({},({},{},{}))".format(self[5].value(), self[7].value(), self[1].value(), self[3].value())
+
+class Trig (Grammar):
+    grammar = (L('\\sin') | L('\\cos') | L('\\tan'), REF('BracketExpr'))
+
+    def value(self):
+        return self[0].string[1:] + self[1].value()
 
 class ParsedExpr (Grammar):
-    grammar = Frac | Integral | Sum
+    grammar = Frac | Integral | Sum | Trig
 
     def value(self):
         return self[0].value()
@@ -81,7 +94,7 @@ class ParenExpr (Grammar):
     grammar = (L('('), REF('Expr'), L(')'))
 
     def value(self):
-        return self.string
+        return "(" + self[1].value() + ")"
 
 class BracketExpr (Grammar):
     grammar = (L('{'), REF('Expr'), L('}'))
@@ -107,6 +120,7 @@ class Var (Grammar):
     grammar = (OPTIONAL('-'), WORD('a-z') | WORD('A-Z'))
 
     def value(self):
+        symParser.add_sym_var(self[1].string)
         return self.string
 
 class SingleTon (Grammar):
@@ -201,8 +215,12 @@ if __name__ == '__main__':
     parser = Expr.parser()
     result = parser.parse_text(sys.argv[1], eof=True)
     remainder = parser.remainder()
+
     print("Parsed Text: {}".format(result))
     print("Unparsed Text: {}".format(remainder))
     print("Value: {}".format(result.value()))
+    symParser.create_symvars()
+    simple = symParser.simplify(result.value())
+    print("Simplify: {}".format(simple))
 
 
